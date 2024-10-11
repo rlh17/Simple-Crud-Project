e3b0110ab2172a8efc69f842d8c6c73407efaef4
rlh17 <richardlharris17@gmail.com>
2024-10-11 16:58:12 +0530

    changing class name
    Changing class name in ServletInitizer.java
    
    DemoJpaApplication.class -> SimpleCrudApplication.class

--- Simple CRUD Project/src/main/java/com/flux/ServletInitializer.java
+++ Simple CRUD Project/src/main/java/com/flux/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoJpaApplication.class);
+		return application.sources(SimpleCrudApplication.class);
 	}
 
 }
